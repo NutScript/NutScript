5c61d40644d187a1c684b6e35c74288119a5085b
Zoephix <heijmanszep@gmail.com>
2021-02-15 13:49:04 +0100

    Added credits tab

--- plugins/f1menu/derma/cl_helps.lua
+++ plugins/f1menu/derma/cl_helps.lua
@@ -1,4 +1,10 @@
 if (CLIENT) then
+	surface.CreateFont("nshtml", {
+		font = "Jeju Gothic",
+		size = 25,
+		weight = 600
+	})
+
 	local HELP_DEFAULT
 
 	hook.Add("CreateMenuButtons", "nutHelpMenu", function(tabs)		
@@ -49,6 +55,19 @@ if (CLIENT) then
 				if (node.onGetHTML) then
 					local source = node:onGetHTML()
 
+					if (IsValid(cAvatar)) then
+						helpFrame:Remove()
+
+						helpFrame = panel:Add("DPanel")
+						helpFrame:Dock(FILL)
+						helpFrame.Paint = function(this, w, h)
+						end
+
+						html = helpFrame:Add("DHTML")
+						html:Dock(FILL)
+						html:SetHTML(header..HELP_DEFAULT)
+					end
+
 					if (source:sub(1, 4) == "http") then
 						html:OpenURL(source)
 					else
@@ -57,7 +76,12 @@ if (CLIENT) then
 				end
 			end
 
-			html = panel:Add("DHTML")
+			helpFrame = panel:Add("DPanel")
+			helpFrame:Dock(FILL)
+			helpFrame.Paint = function(this, w, h)
+			end
+
+			html = helpFrame:Add("DHTML")
 			html:Dock(FILL)
 			html:SetHTML(header..HELP_DEFAULT)
 
@@ -112,28 +136,6 @@ hook.Add("BuildHelpMenu", "nutBasicHelp", function(tabs)
 		return body
 	end
 
-	tabs["plugins"] = function(node)
-		local body = ""
-
-		for k, v in SortedPairsByMemberValue(nut.plugin.list, "name") do
-			body = (body..[[
-				<p>
-					<span style="font-size: 22;"><b>%s</b><br /></span>
-					<span style="font-size: smaller;">
-					<b>%s</b>: %s<br />
-					<b>%s</b>: %s
-			]]):format(v.name or "Unknown", L"desc", v.desc or L"noDesc", L"author", v.author)
-
-			if (v.version) then
-				body = body.."<br /><b>"..L"version".."</b>: "..v.version
-			end
-
-			body = body.."</span></p>"
-		end
-
-		return body
-	end
-
 	tabs["flags"] = function(node)
 		local body = [[<table border="0" cellspacing="8px">]]
 
@@ -156,5 +158,89 @@ hook.Add("BuildHelpMenu", "nutBasicHelp", function(tabs)
 		end
 
 		return body.."</table>"
+	end
+
+	tabs["plugins"] = function(node)
+		local body = ""
+
+		for k, v in SortedPairsByMemberValue(nut.plugin.list, "name") do
+			body = (body..[[
+				<p>
+					<span style="font-size: 22;"><b>%s</b><br /></span>
+					<span style="font-size: smaller;">
+					<b>%s</b>: %s<br />
+					<b>%s</b>: %s
+			]]):format(v.name or "Unknown", L"desc", v.desc or L"noDesc", L"author", v.author)
+
+			if (v.version) then
+				body = body.."<br /><b>"..L"version".."</b>: "..v.version
+			end
+
+			body = body.."</span></p>"
+		end
+
+		return body
+	end
+
+	local authorCredits = {
+		{desc = "Creator", steamid = "76561198030127257"}, -- Chessnut
+		{desc = "Co-Creator", steamid = "76561197999893894"}, -- Black Tea
+		{desc = "Developer", steamid = "76561198060659964"}, -- Zoephix
+		{desc = "Developer", steamid = "76561198070441753"} -- TovarischPootis
+	}
+
+	tabs["Credits"] = function(node)
+		local body = ""
+		local scrW = ScrW()
+		local scrH = ScrH()
+		local offsetW = scrW*0.025
+		local offsetH = scrH*0.06
+		
+		local title = helpFrame:Add("DLabel")
+		title:SetText("NutScript")
+		title:SetFont("nutBigFont")
+		title:SetTextColor(Color(86,111,183))
+		title:SizeToContents()
+		title:Dock(TOP)
+
+		local nscredits = helpFrame:Add("DPanel")
+		nscredits:Dock(FILL)
+		nscredits:DockMargin(0, 7.5, 0, 0)
+		nscredits.Paint = function(this, w, h)
+		end
+
+		for k, v in ipairs(authorCredits) do
+			local offsetH = k == 1 and 0 or (k*offsetH)-offsetH
+
+			steamworks.RequestPlayerInfo( v.steamid, function(steamName)
+				local name = nscredits:Add("DLabel")
+				name:SetText(steamName)
+				name:SetPos(offsetW, offsetH)
+				name:SetFont("nshtml")
+				name:SetTextColor(Color(255, 255, 255))
+				name:SizeToContents()
+			end )
+
+			local desc = nscredits:Add("DLabel")
+			desc:SetText(v.desc)
+			desc:SetPos(offsetW, offsetH+(scrH*0.022))
+			desc:SetFont("nutSmallFont")
+			desc:SetTextColor(Color(255, 255, 255))
+			desc:SizeToContents()
+
+			cAvatar = vgui.Create("AvatarImage", nscredits)
+			cAvatar:SetPos(0, offsetH)
+			cAvatar:SetSteamID(v.steamid, 64)
+			cAvatar:SetSize(40, 40)
+			cAvatar.OnMousePressed = function(self)
+				surface.PlaySound("buttons/button14.wav")
+				
+				gui.OpenURL("http://steamcommunity.com/profiles/"..v.steamid)
+			end
+
+			body = body.."</span></p>"
+		end
+
+		return body
 	end
 end)
