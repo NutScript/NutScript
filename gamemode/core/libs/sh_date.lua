f863518115307d50b0492ca9d459b13a422683e4
Zoephix <heijmanszep@gmail.com>
2021-02-17 13:16:55 +0100

    1.2 wip (#24)
    * Add persistence plugin
    
    * lua consistency
    
    newlines and || vs or
    
    * Removed restrictions plugin alliances
    
    * Update sv_database.lua
    
    * whitespace
    
    * Update sh_plugin.lua
    
    * Revert "Merge pull request #10 from Zoephix/1.2-wip"
    
    This reverts commit 944229f4eb9fc74af5dba8259c689ba28881e243.
    
    * Revert "Update sh_plugin.lua"
    
    This reverts commit 57cc63e3b3d687afc9fae6b88f9cdb732b2b3ed6.
    
    * yet another date modification, this time, lightweight
    
    * Removed already defined starting time
    
    * Update database configuration loading
    
    * Added date migrations
    
    * Added date migrations
    
    * Update chatbox icons
    
    * As suggested, timer runs only on day change rather than every 5 minutes
    
    * Update README.md
    
    * doesnt exist anymore
    
    * Added more valid config locations
    
    * Update config loading
    
    * Update config loading
    
    * Update config loading
    
    * Added example database configuration
    
    * Added credits tab
    
    * Added hover sound
    
    * whitespace
    
    * Credits tab (WIP)
    
    * Added contributors to the credits
    
    * Increased spacing, extra polishing
    
    * Increased font size
    
    Co-authored-by: TovarischPootis <54110479+TovarischPootis@users.noreply.github.com>

--- gamemode/core/libs/sh_date.lua
+++ gamemode/core/libs/sh_date.lua
@@ -1,178 +1,112 @@
----------------------------------------------------------------------------------------
 -- Module for date and time calculations
---
--- Version 2.1.2
--- Copyright (C) 2006, by Jas Latrix (jastejada@yahoo.com)
--- Copyright (C) 2013-2014, by Thijs Schreijer
--- Licensed under MIT, http://opensource.org/licenses/MIT
--- https://github.com/Tieske/date
--- The MIT License (MIT) http://opensource.org/licenses/MIT
-
--- Copyright (c) 2013-2017 Thijs Schreijer
--- Copyright (c) 2018 Alexander Grist-Hucker, Igor Radovanovic
-
--- lib based on Helix by Alexander Grist-Hucker, Igor Radovanovic 2018
-
--- due to UNIX time, normal os.time() cannnot be set before 1970, as 1st Jan 1970 is 0
--- the library fixes said issue. 
-
 
 nut.date = nut.date or {}
-nut.date.lib = nut.date.lib or include("thirdparty/sh_date.lua")
-nut.date.timeScale = nut.date.timeScale or nut.config.get("secondsPerMinute", 60)
-nut.date.dateObj = nut.date.dateObj or nut.date.lib()
-nut.date.start = nut.date.start or CurTime()
 
 if (not nut.config) then
-	include("nutscript/gamemode/core/sh_config.lua")
+    include("nutscript/gamemode/core/sh_config.lua")
 end
 
-nut.config.add("year", 2021, "The starting year of the schema.", function(oldValue, newValue)
-	if (SERVER and not nut.date.saving) then
-		nut.date.update()
-		nut.date.dateObj:setyear(newValue)
-		nut.date.sync()
-	end
-end, {
-	data = {min = 0, max = 4000},
-	category = "date"
-})
+nut.config.add("year", tonumber(os.date("%Y")), "The current year of the schema." , nil, {
+    data = {min = 0, max = 4000},
+    category = "date"
+}
+)
 
-nut.config.add("month", 1, "The starting month of the schema.", function(oldValue, newValue)
-	if (SERVER and not nut.date.saving) then
-		nut.date.update()
-		nut.date.dateObj:setmonth(newValue)
-		nut.date.sync()
-	end
-end, {
-	data = {min = 1, max = 12},
-	category = "date"
-})
+nut.config.add("month", tonumber(os.date("%m")), "The current month of the schema." , nil, {
+    data = {min = 1, max = 12},
+    category = "date"
+}
+)
 
-nut.config.add("day", 1, "The starting day of the schema.", function(oldValue, newValue)
-	if (SERVER and not nut.date.saving) then
-		nut.date.update()
-		nut.date.dateObj:setday(newValue)
-		nut.date.sync()
-	end
-end, {
-	data = {min = 1, max = 31},
-	category = "date"
-})
-
-nut.config.add("secondsPerMinute", 60, "How many real life seconds it takes for a minute to pass in-game.", function(oldValue, newValue)
-	if (SERVER and not nut.date.saving) then
-		nut.date.updateTimescale(newValue)
-		nut.date.sync()
-	end
-end, {
-	data = {min = 0.01, max = 120},
-	category = "date"
-})
+nut.config.add("day", tonumber(os.date("%d")), "The current day of the schema." , nil, {
+    data = {min = 1, max = 31},
+    category = "date"
+}
+)
 
 nut.config.add("yearAppendix", "", "Add a custom appendix to your date, if you use a non-conventional calender", nil, {
 	data = {form = "Generic"},
 	category = "date"
-})
+}
+)
 
-if (SERVER) then
-	util.AddNetworkString("nutDateSync")
+-- function returns a number that represents the custom time. the year is always the current year for 
+-- compatibility, though it can be editted with nut.date.getFormatted
 
-	-- called upon server startup. Grabs the saved date data, or creates a new date instance, and sets it as the date object
-	function nut.date.initialize()
-		local currentDate = nut.data.get("date", nil, false, true)
-		-- If we don't have date data already, use current defaults to create a new date data table
-		if (not currentDate) then
-			currentDate = {
-				year = nut.config.get("year"),
-				month = nut.config.get("month"),
-				day = nut.config.get("day"),
-				hour = tonumber(os.date("%H")) or 0,
-				min = tonumber(os.date("%M")) or 0,
-				sec = tonumber(os.date("%S")) or 0,
-			}
-
-			currentDate = nut.date.lib.serialize(nut.date.lib(currentDate))
-
-			nut.data.set("date", currentDate, false, true) -- save the new data
-		end
-
-		nut.date.timeScale = nut.config.get("secondsPerMinute", 60)
-		nut.date.dateObj = nut.date.lib.construct(currentDate) -- update the date object with the initialized data
-	end
-
-	-- Called when date values have been manually changed, updating the date object.
-	function nut.date.update()
-		nut.date.dateObj = nut.date.get()
-		nut.date.start = CurTime()
-	end
-
-	-- This is an internal function that sets the amount of real life seconds in an ingame minute. While you can use this function,
-	--you probably shouldn't, and rather use the ingame config.
-	function nut.date.updateTimescale(secondsPerMinute)
-		nut.date.update()
-		nut.date.timeScale = secondsPerMinute
-	end
-
-	--Syncs the current date with the client/s. This allows the players to have proper date representation, such as in the F1menu.
-	function nut.date.sync(client)
-		net.Start("nutDateSync")
-		net.WriteFloat(nut.date.timeScale)
-		net.WriteTable(nut.date.dateObj)
-		net.WriteFloat(nut.date.start)
-
-		if (client) then
-			net.Send(client)
-		else
-			net.Broadcast()
-		end
-	end
-
-	-- saves the current in-game date data.
-	function nut.date.save()
-		nut.date.saving = true -- prevents from the function from being called before it finishes.
-
-		nut.date.update()
-
-		nut.data.set("date", nut.date.lib.serialize(nut.date.dateObj), false, true) -- saves the current data object
-
-		-- update config to reflect current saved date
-		nut.config.set("year", nut.date.dateObj:getyear())
-		nut.config.set("month", nut.date.dateObj:getmonth())
-		nut.config.set("day", nut.date.dateObj:getday())
-
-		nut.date.saving = nil -- allows the date to be saved again
-	end
-else
-	net.Receive("nutDateSync", function() -- set the clientside values to the updated serverside date values 
-		nut.date.timeScale = net.ReadFloat()
-		nut.date.dateObj = nut.date.lib.construct(net.ReadTable())
-		nut.date.start = net.ReadFloat()
-		print("synced")
-		PrintTable(nut.date.dateObj)
-	end)
-end
-
---- Returns the currently set date.
 function nut.date.get()
-	-- CurTime increases in value by 1 every second. By getting the difference in seconds between now and the date object initialization,
-	local minutesSinceStart = (CurTime() - nut.date.start) / nut.date.timeScale --and divide it by the timescale, we get the minutes elapsed to add to the date start
-
-	return nut.date.dateObj:copy():addminutes(minutesSinceStart)
+	return os.time({
+        year=os.date("%Y"),
+        month=nut.config.get("month"),
+        day=nut.config.get("day"),
+        hour=os.date("%H"),
+        min=os.date("%M"),
+        sec=os.date("%S")
+    })
 end
 
---- Returns a string formatted version of a date.
-function nut.date.getFormatted(format, currentDate)
-	return (currentDate or nut.date.get()):fmt(format)
+--function takes the time number if provided, or current time and applies a string format to it
+
+function nut.date.getFormatted(format, dateNum)
+	return os.date(format, dateNum or nut.date.get())
 end
 
 if SERVER then
-	hook.Add("InitializedSchema", "nutInitializeTime", function()
-		nut.date.initialize()
-	end)
 
-	hook.Add("PlayerInitialSpawn", "nutDateSync", function(client)
-		nut.date.sync(client)
+	-- This is internal, though you can use it you probably shouldn't. 
+	-- Checks the time difference between the old time values and current time, and updates month and day to advance in the time difference
+	-- creates a timer that updates the month and day values, in case the server runs continuously without restarts.
+	function nut.date.initialize()
+
+		-- Migrations
+		if (istable(nut.data.get("date", os.time(), true))) then
+			nut.data.set("date", os.time(), true, true)
+		end
+
+		local configTime = os.time({
+			year = tonumber(os.date("%Y")),
+			month = tonumber(nut.config.get("month")),
+			day = tonumber(nut.config.get("day")),
+			hour = tonumber(os.date("%H")),
+			min = os.date("%M"),
+        	sec = os.date("%S")
+		}) + os.difftime(os.time(), nut.data.get("date", os.time(), true))
+
+		nut.config.set("month", tonumber(os.date("%m", configTime)))
+		nut.config.set("day", tonumber(os.date("%d", configTime)))
+
+		-- internal function that calculates when the day ends, and updates the month/day when the next day comes.
+		-- the reason for this complication instead of just upvaluing day/month by 1 is that some months have 28, 30 or 31 days.
+		-- and its simpler for the server to decide what the next month should be rather than manually computing that
+		local function updateDateConfigs()
+			local curDateTable = os.date("*t") -- get the current date table
+			local remainingSeconds = (curDateTable.hour * -3600 - curDateTable.min * 60 - curDateTable.sec) % 86400 -- get the remaining seconds until the new day
+
+			timer.Simple(remainingSeconds, function() -- run this code only once the day changes
+				local newTime = os.time({
+					year = tonumber(os.date("%Y")),
+					month = tonumber(nut.config.get("month")),
+					day = tonumber(nut.config.get("day")),
+					hour = tonumber(os.date("%H")),
+					min = os.date("%M"),
+					sec = os.date("%S")
+				}) + 86400 -- 24 hours.
+
+				nut.config.set("month", tonumber(os.date("%m", newTime)))
+				nut.config.set("day", tonumber(os.date("%d", newTime)))
+				updateDateConfigs() -- create a new timer for the next day
+			end)
+		end
+
+		updateDateConfigs()
+	end
+
+	-- saves the current actual time. This allows the time to find the difference in elapsed time between server shutdown and startup
+	function nut.date.save()
+		nut.data.set("date", os.time(), true, true)
+	end
+
+	hook.Add("InitializedConfig", "nutInitializeTime", function()
+		nut.date.initialize()
 	end)
 
 	hook.Add("SaveData", "nutDateSave", function()
--- plugins/f1menu/derma/cl_helps.lua
+++ plugins/f1menu/derma/cl_helps.lua
@@ -1,4 +1,16 @@
-if (CLIENT) then
+
+	surface.CreateFont("nutSmallCredits", {
+		font = "Roboto",
+		size = 18,
+		weight = 400
+	})
+
+	surface.CreateFont("nutBigCredits", {
+		font = "Roboto",
+		size = 25,
+		weight = 600
+	})
+
 	local HELP_DEFAULT
 
 	hook.Add("CreateMenuButtons", "nutHelpMenu", function(tabs)		
@@ -49,6 +61,19 @@ if (CLIENT) then
 				if (node.onGetHTML) then
 					local source = node:onGetHTML()
 
+					if (IsValid(devAvatar)) then
+						helpPanel:Remove()
+
+						helpPanel = panel:Add("DListView")
+						helpPanel:Dock(FILL)
+						helpPanel.Paint = function(this, w, h)
+						end
+
+						html = helpPanel:Add("DHTML")
+						html:Dock(FILL)
+						html:SetHTML(header..HELP_DEFAULT)
+					end
+
 					if (source:sub(1, 4) == "http") then
 						html:OpenURL(source)
 					else
@@ -57,7 +82,12 @@ if (CLIENT) then
 				end
 			end
 
-			html = panel:Add("DHTML")
+			helpPanel = panel:Add("DListView")
+			helpPanel:Dock(FILL)
+			helpPanel.Paint = function(this, w, h)
+			end
+
+			html = helpPanel:Add("DHTML")
 			html:Dock(FILL)
 			html:SetHTML(header..HELP_DEFAULT)
 
@@ -75,7 +105,6 @@ if (CLIENT) then
 			end
 		end
 	end)
-end
 
 hook.Add("BuildHelpMenu", "nutBasicHelp", function(tabs)
 	tabs["commands"] = function(node)
@@ -112,28 +141,6 @@ hook.Add("BuildHelpMenu", "nutBasicHelp", function(tabs)
 		return body
 	end
 
-	tabs["plugins"] = function(node)
-		local body = ""
-
-		for k, v in SortedPairsByMemberValue(nut.plugin.list, "name") do
-			body = (body..[[
-				<p>
-					<span style="font-size: 22;"><b>%s</b><br /></span>
-					<span style="font-size: smaller;">
-					<b>%s</b>: %s<br />
-					<b>%s</b>: %s
-			]]):format(v.name or "Unknown", L"desc", v.desc or L"noDesc", L"author", v.author)
-
-			if (v.version) then
-				body = body.."<br /><b>"..L"version".."</b>: "..v.version
-			end
-
-			body = body.."</span></p>"
-		end
-
-		return body
-	end
-
 	tabs["flags"] = function(node)
 		local body = [[<table border="0" cellspacing="8px">]]
 
@@ -156,5 +163,126 @@ hook.Add("BuildHelpMenu", "nutBasicHelp", function(tabs)
 		end
 
 		return body.."</table>"
+	end
+
+	tabs["plugins"] = function(node)
+		local body = ""
+
+		for k, v in SortedPairsByMemberValue(nut.plugin.list, "name") do
+			body = (body..[[
+				<p>
+					<span style="font-size: 22;"><b>%s</b><br /></span>
+					<span style="font-size: smaller;">
+					<b>%s</b>: %s<br />
+					<b>%s</b>: %s
+			]]):format(v.name or "Unknown", L"desc", v.desc or L"noDesc", L"author", v.author)
+
+			if (v.version) then
+				body = body.."<br /><b>"..L"version".."</b>: "..v.version
+			end
+
+			body = body.."</span></p>"
+		end
+
+		return body
+	end
+
+	local authorCredits = {
+		{desc = "Creator", steamid = "76561198030127257"}, -- Chessnut
+		{desc = "Co-Creator", steamid = "76561197999893894"}, -- Black Tea
+		{desc = "Developer", steamid = "76561198060659964"}, -- Zoephix
+		{desc = "Developer", steamid = "76561198070441753"}, -- TovarischPootis
+		{desc = "Contributors", button = "View all contributors", url = "https://github.com/NutScript/NutScript/graphs/contributors"}
+	}
+
+	tabs["Credits"] = function(node)
+		local body = [[
+			<div>
+				<center>
+				    <img src="http://img2.wikia.nocookie.net/__cb20140827051941/nutscript/images/c/c9/Logo.png"></img>
+					<br><font size=25>NutScript</font>
+				</center>
+			</div>
+		]]
+		local scrW = ScrW()
+		local scrH = ScrH()
+		local offsetW = scrW*0.025
+		local offsetH = scrH*0.08
+		local offsetC = scrW*0.25
+
+		local nscredits = helpPanel:Add("DScrollPanel")
+		nscredits:Dock(FILL)
+		nscredits:DockMargin(0, scrH*0.32, 0, 0)
+		nscredits.Paint = function()
+		end
+
+		local function createDevName(text)
+			devName = nscredits:Add("DLabel")
+			devName:SetText(text)
+			devName:SetFont("nutBigCredits")
+			devName:SetTextColor(Color(255, 255, 255))
+			devName:SizeToContents()
+		end
+
+		for k, v in ipairs(authorCredits) do
+			local offsetH = k == 1 and 0 or (k*offsetH)-offsetH
+
+			if (v.steamid) then
+				steamworks.RequestPlayerInfo(v.steamid, function(steamName)
+					createDevName(steamName)
+				end)
+			--
+			-- add custom field
+			else
+				createDevName(v.desc)
+			end
+
+			if (v.steamid or v.text) then
+				desc = nscredits:Add("DLabel")
+				desc:SetText(v.desc)
+				desc:SetFont("nutSmallCredits")
+				desc:SetTextColor(Color(255, 255, 255))
+				desc:SizeToContents()
+			elseif (v.button) then
+				desc = nscredits:Add("DButton")
+				desc:SetText(v.button)
+				desc:SizeToContents()
+				desc.DoClick = function()
+					surface.PlaySound("buttons/button14.wav")
+
+					gui.OpenURL(v.url)
+				end
+
+				offsetH = offsetH + 25
+			end
+
+			-- fucking end my suffering
+			-- this shit took me over three hours...
+			local offsetW2 = string.Explode(" ", v.button and desc:GetSize() or desc:GetTextSize())[1]
+			local offsetW3 = string.Explode(" ", devName:GetTextSize())[1]
+
+			devName:SetPos(devName:GetPos()+(offsetC-devName:GetPos()), 0)
+			devName:SetPos(devName:GetPos()-(offsetW3/2), offsetH)
+
+			desc:SetPos(desc:GetPos()+(offsetC-desc:GetPos()), 0)
+			desc:SetPos(desc:GetPos()-(offsetW2/2), offsetH+(v.button and scrH*0.03 or scrH*0.022))
+
+			if (v.steamid) then
+				devAvatar = vgui.Create("AvatarImage", nscredits)
+				devAvatar:SetPos(devName:GetPos()-(scrW*0.025), offsetH)
+				devAvatar:SetSteamID(v.steamid, 64)
+				devAvatar:SetSize(40, 40)
+				devAvatar.OnCursorEntered = function()
+					surface.PlaySound("garrysmod/ui_return.wav")
+				end
+				devAvatar.OnMousePressed = function()
+					surface.PlaySound("buttons/button14.wav")
+
+					gui.OpenURL("http://steamcommunity.com/profiles/"..v.steamid)
+				end
+			end
+		end
+
+		return body
 	end
 end)
