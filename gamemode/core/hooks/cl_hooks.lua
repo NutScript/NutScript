e29f6f04ddecd89f745353b2ad47c0ce2b61779e
Zoephix <heijmanszep@gmail.com>
2021-02-17 17:21:51 +0100

    snip

--- gamemode/core/hooks/cl_hooks.lua
+++ gamemode/core/hooks/cl_hooks.lua
@@ -541,9 +541,8 @@ end
 function GM:NutScriptLoaded()
 	for _, PLUGIN in pairs(nut.plugin.list) do
 		local author64ID = tonumber(PLUGIN.author)
-		local authorSteamID = string.match(PLUGIN.author, "STEAM_", 1)
 
-		if (author64ID or authorSteamID) then
+		if (author64ID or string.match(PLUGIN.author, "STEAM_", 1)) then
 			-- Store the Steam ID
 			PLUGIN.authorID = author64ID and util.SteamIDFrom64(PLUGIN.author) or PLUGIN.author
 
