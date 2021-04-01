e92ca59f76461075c7734c0aa6dd2f27a7d30c23
TovarischPootis <54110479+TovarischPootis@users.noreply.github.com>
2021-04-01 18:58:06 +0300

    Remade Credits. Added surfaceGetURL. Added comments to ease to explain how to use.

--- plugins/f1menu/derma/cl_helps.lua
+++ plugins/f1menu/derma/cl_helps.lua
@@ -1,23 +1,11 @@
 
-	surface.CreateFont("nutSmallCredits", {
-		font = "Roboto",
-		size = 18,
-		weight = 400
-	})
-
-	surface.CreateFont("nutBigCredits", {
-		font = "Roboto",
-		size = 25,
-		weight = 600
-	})
-
 	local HELP_DEFAULT
 
-	hook.Add("CreateMenuButtons", "nutHelpMenu", function(tabs)		
+	hook.Add("CreateMenuButtons", "nutHelpMenu", function(tabs)
 		HELP_DEFAULT = [[
 			<div id="parent"><div id="child">
 				<center>
-				    <img src="http://img2.wikia.nocookie.net/__cb20140827051941/nutscript/images/c/c9/Logo.png"></img>
+				    <img src="https://static.miraheze.org/nutscriptwiki/2/26/Nutscript.png"></img>
 					<br><font size=15>]] .. L"helpDefault" .. [[</font>
 				</center>
 			</div></div>
@@ -60,21 +48,25 @@
 			tree.OnNodeSelected = function(this, node)
 				if (node.onGetHTML) then
 					local source = node:onGetHTML()
-
-					if (IsValid(devAvatar)) then
+					if IsValid(helpPanel) then
+						print("this triggered")
 						helpPanel:Remove()
-
-						helpPanel = panel:Add("DListView")
-						helpPanel:Dock(FILL)
-						helpPanel.Paint = function(this, w, h)
-						end
-
-						html = helpPanel:Add("DHTML")
-						html:Dock(FILL)
-						html:SetHTML(header..HELP_DEFAULT)
+					end
+					if nut.gui.creditsPanel then
+						nut.gui.creditsPanel:Remove()
 					end
 
-					if (source:sub(1, 4) == "http") then
+					helpPanel = panel:Add("DListView")
+					helpPanel:Dock(FILL)
+					helpPanel.Paint = function()
+					end
+					helpPanel:InvalidateLayout(true)
+
+					html = helpPanel:Add("DHTML")
+					html:Dock(FILL)
+					html:SetHTML(header..HELP_DEFAULT)
+
+					if (source and source:sub(1, 4) == "http") then
 						html:OpenURL(source)
 					else
 						html:SetHTML(header..node:onGetHTML().."</body></html>")
@@ -82,16 +74,17 @@
 				end
 			end
 
-			helpPanel = panel:Add("DListView")
-			helpPanel:Dock(FILL)
-			helpPanel.Paint = function(this, w, h)
+			if not IsValid(helpPanel) then
+				helpPanel = panel:Add("DListView")
+				helpPanel:Dock(FILL)
+				helpPanel.Paint = function()
+				end
+
+				html = helpPanel:Add("DHTML")
+				html:Dock(FILL)
+				html:SetHTML(header..HELP_DEFAULT)
 			end
-
-			html = helpPanel:Add("DHTML")
-			html:Dock(FILL)
-			html:SetHTML(header..HELP_DEFAULT)
-
-			local tabs = {}
+			tabs = {}
 			hook.Run("BuildHelpMenu", tabs)
 
 			for k, v in SortedPairs(tabs) do
@@ -113,14 +106,14 @@ hook.Add("BuildHelpMenu", "nutBasicHelp", function(tabs)
 		for k, v in SortedPairs(nut.command.list) do
 			local allowed = false
 
-			if (v.adminOnly and !LocalPlayer():IsAdmin()or v.superAdminOnly and !LocalPlayer():IsSuperAdmin()) then
+			if (v.adminOnly and not LocalPlayer():IsAdmin()or v.superAdminOnly and not LocalPlayer():IsSuperAdmin()) then
 				continue
 			end
 
 			if (v.group) then
 				if (istable(v.group)) then
-					for k, v in pairs(v.group) do
-						if (LocalPlayer():IsUserGroup(v)) then
+					for _, v1 in pairs(v.group) do
+						if (LocalPlayer():IsUserGroup(v1)) then
 							allowed = true
 
 							break
@@ -168,7 +161,7 @@ hook.Add("BuildHelpMenu", "nutBasicHelp", function(tabs)
 	tabs["plugins"] = function(node)
 		local body = ""
 
-		for k, v in SortedPairsByMemberValue(nut.plugin.list, "name") do
+		for _, v in SortedPairsByMemberValue(nut.plugin.list, "name") do
 			body = (body..[[
 				<p>
 					<span style="font-size: 22;"><b>%s</b><br /></span>
@@ -182,105 +175,6 @@ hook.Add("BuildHelpMenu", "nutBasicHelp", function(tabs)
 			end
 
 			body = body.."</span></p>"
-		end
-
-		return body
-	end
-
-	local authorCredits = {
-		{desc = "Creator", steamid = "76561198030127257"}, -- Chessnut
-		{desc = "Co-Creator", steamid = "76561197999893894"}, -- Black Tea
-		{desc = "Developer", steamid = "76561198060659964"}, -- Zoephix
-		{desc = "Developer", steamid = "76561198070441753"}, -- TovarischPootis
-		{desc = "Contributors", button = "View all contributors", url = "https://github.com/NutScript/NutScript/graphs/contributors"}
-	}
-
-	tabs["Credits"] = function(node)
-		local body = [[
-			<div>
-				<center>
-				    <img src="http://img2.wikia.nocookie.net/__cb20140827051941/nutscript/images/c/c9/Logo.png"></img>
-					<br><font size=25>NutScript</font>
-				</center>
-			</div>
-		]]
-		local scrW = ScrW()
-		local scrH = ScrH()
-		local offsetW = scrW*0.025
-		local offsetH = scrH*0.08
-		local offsetC = scrW*0.25
-
-		local nscredits = helpPanel:Add("DScrollPanel")
-		nscredits:Dock(FILL)
-		nscredits:DockMargin(0, scrH*0.32, 0, 0)
-		nscredits.Paint = function()
-		end
-
-		for k, v in ipairs(authorCredits) do
-			local offsetH = k == 1 and 0 or (k*offsetH)-offsetH
-
-			if (v.steamid) then
-				steamworks.RequestPlayerInfo(v.steamid, function(steamName)
-					devName = nscredits:Add("DLabel")
-					devName:SetText(steamName)
-					devName:SetFont("nutBigCredits")
-					devName:SetTextColor(Color(255, 255, 255))
-					devName:SizeToContents()
-				end)
-			--
-			-- add custom field
-			else
-				devName = nscredits:Add("DLabel")
-				devName:SetText(v.desc)
-				devName:SetFont("nutBigCredits")
-				devName:SetTextColor(Color(255, 255, 255))
-				devName:SizeToContents()
-			end
-
-			if (v.steamid or v.text) then
-				desc = nscredits:Add("DLabel")
-				desc:SetText(v.desc)
-				desc:SetFont("nutSmallCredits")
-				desc:SetTextColor(Color(255, 255, 255))
-				desc:SizeToContents()
-			elseif (v.button) then
-				desc = nscredits:Add("DButton")
-				desc:SetText(v.button)
-				desc:SizeToContents()
-				desc.DoClick = function()
-					surface.PlaySound("buttons/button14.wav")
-
-					gui.OpenURL(v.url)
-				end
-
-				offsetH = offsetH + 25
-			end
-
-			-- fucking end my suffering
-			-- this shit took me over three hours...
-			local offsetW2 = string.Explode(" ", v.button and desc:GetSize() or desc:GetTextSize())[1]
-			local offsetW3 = string.Explode(" ", devName:GetTextSize())[1]
-
-			devName:SetPos(devName:GetPos()+(offsetC-devName:GetPos()), 0)
-			devName:SetPos(devName:GetPos()-(offsetW3/2), offsetH)
-
-			desc:SetPos(desc:GetPos()+(offsetC-desc:GetPos()), 0)
-			desc:SetPos(desc:GetPos()-(offsetW2/2), offsetH+(v.button and scrH*0.03 or scrH*0.022))
-
-			if (v.steamid) then
-				devAvatar = vgui.Create("AvatarImage", nscredits)
-				devAvatar:SetPos(devName:GetPos()-(scrW*0.025), offsetH)
-				devAvatar:SetSteamID(v.steamid, 64)
-				devAvatar:SetSize(40, 40)
-				devAvatar.OnCursorEntered = function()
-					surface.PlaySound("garrysmod/ui_return.wav")
-				end
-				devAvatar.OnMousePressed = function()
-					surface.PlaySound("buttons/button14.wav")
-
-					gui.OpenURL("http://steamcommunity.com/profiles/"..v.steamid)
-				end
-			end
 		end
 
 		return body
