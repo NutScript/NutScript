7663e1179d92fb2aaf5e510639c89d1361b6bd84
epichl2rpgamer <69685972+epichl2rpgamer@users.noreply.github.com>
2022-03-06 19:46:46 +0100

    Apply BNP fix to the markup library

--- gamemode/core/libs/cl_markup.lua
+++ gamemode/core/libs/cl_markup.lua
@@ -402,7 +402,7 @@ function parse(ml, maxwidth)
 							end
 							
 							local m = 1
-							while string.utf8sub(ch, m, m) == " " do
+							while string.utf8sub(ch, m, m) == " " and m <= string.utf8len(ch) do
 								m = m + 1
 							end
 							ch = string.utf8sub(ch, m)
