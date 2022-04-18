bf762fedaa9b320d0d8eecf6a11f9148cd983e07
Barata <33399712+baratoxis@users.noreply.github.com>
2022-04-18 16:15:32 +0100

    Added Automatic Class Saving
    Added Automatic Class Saving to the Config

--- gamemode/config/sh_config.lua
+++ gamemode/config/sh_config.lua
@@ -10,174 +10,231 @@ nut.config.itemFormat = "<font=nutGenericFont>%s</font>\n<font=nutSmallFont>%s</
 	To set the configuration, there is a "Config" tab in the F1 menu for super admins and above.
 	Use the menu to change the variables, not this file.
 --]]
-
 nut.config.add("maxChars", 5, "The maximum number of characters a player can have.", nil, {
-	data = {min = 1, max = 50},
-	category = "characters"
+    data = {
+        min = 1,
+        max = 50
+    },
+    category = "characters"
 })
 
-nut.config.add("color", Color(75, 119, 190), "The main color theme for the framework.", nil, {category = "appearance"})
+nut.config.add("class_saving", false, "Enables/Disables the automatic saving of classes on a player.", nil, {
+    category = "characters"
+})
+
+nut.config.add("color", Color(75, 119, 190), "The main color theme for the framework.", nil, {
+    category = "appearance"
+})
 
 nut.config.add("font", "Arial", "The font used to display titles.", function(oldValue, newValue)
-	if (CLIENT) then
-		hook.Run("LoadNutFonts", newValue, nut.config.get("genericFont"))
-	end
-end, {category = "appearance"})
+    if (CLIENT) then
+        hook.Run("LoadNutFonts", newValue, nut.config.get("genericFont"))
+    end
+end, {
+    category = "appearance"
+})
 
 nut.config.add("genericFont", "Segoe UI", "The font used to display generic texts.", function(oldValue, newValue)
-	if (CLIENT) then
-		hook.Run("LoadNutFonts", nut.config.get("font"), newValue)
-	end
-end, {category = "appearance"})
+    if (CLIENT) then
+        hook.Run("LoadNutFonts", nut.config.get("font"), newValue)
+    end
+end, {
+    category = "appearance"
+})
 
 nut.config.add("fontScale", 1.0, "The scale for the font.", function(oldValue, newValue)
-	if (CLIENT) then
-		hook.Run("LoadNutFonts", nut.config.get("font"), nut.config.get("genericFont"))
-	end
+    if (CLIENT) then
+        hook.Run("LoadNutFonts", nut.config.get("font"), nut.config.get("genericFont"))
+    end
 end, {
-	form = "Float",
-	data = {min = 0.1, max = 2.0},
-	category = "appearance"
+    form = "Float",
+    data = {
+        min = 0.1,
+        max = 2.0
+    },
+    category = "appearance"
 })
 
 nut.config.add("chatRange", 280, "The maximum distance a person's IC chat message goes to.", nil, {
-	form = "Float",
-	data = {min = 10, max = 5000},
-	category = "chat"
+    form = "Float",
+    data = {
+        min = 10,
+        max = 5000
+    },
+    category = "chat"
 })
 
-nut.config.add("chatColor", Color(255, 239, 150), "The default color for IC chat.", nil, {category = "chat"})
+nut.config.add("chatColor", Color(255, 239, 150), "The default color for IC chat.", nil, {
+    category = "chat"
+})
 
-nut.config.add("chatListenColor", Color(168, 240, 170), "The color for IC chat if you are looking at the speaker.", nil, {category = "chat"})
+nut.config.add("chatListenColor", Color(168, 240, 170), "The color for IC chat if you are looking at the speaker.", nil, {
+    category = "chat"
+})
 
 nut.config.add("oocDelay", 10, "The delay before a player can use OOC chat again in seconds.", nil, {
-	data = {min = 0, max = 10000},
-	category = "chat"
+    data = {
+        min = 0,
+        max = 10000
+    },
+    category = "chat"
 })
 
 nut.config.add("oocLimit", 0, "Character limit per OOC message. 0 means no limit", nil, {
-	data = {min = 0, max = 1000},
-	category = "chat"
+    data = {
+        min = 0,
+        max = 1000
+    },
+    category = "chat"
 })
 
 nut.config.add("oocDelayAdmin", false, "Whether or not OOC chat delay is enabled for admins.", nil, {
-	category = "chat"
+    category = "chat"
 })
 
 nut.config.add("allowGlobalOOC", true, "Whether or not Global OOC is enabled.", nil, {
-	category = "chat"
+    category = "chat"
 })
 
 nut.config.add("loocDelay", 0, "The delay before a player can use LOOC chat again in seconds.", nil, {
-	data = {min = 0, max = 10000},
-	category = "chat"
+    data = {
+        min = 0,
+        max = 10000
+    },
+    category = "chat"
 })
 
 nut.config.add("loocDelayAdmin", false, "Whether or not LOOC chat delay is enabled for admins.", nil, {
-	category = "chat"
+    category = "chat"
 })
 
 nut.config.add("chatShowTime", false, "Whether or not to show timestamps in front of chat messages.", nil, {
-	category = "chat"
+    category = "chat"
 })
 
 nut.config.add("spawnTime", 5, "The time it takes to respawn.", nil, {
-	data = {min = 0, max = 10000},
-	category = "characters"
+    data = {
+        min = 0,
+        max = 10000
+    },
+    category = "characters"
 })
 
 nut.config.add("invW", 6, "How many slots in a row there is in a default inventory.", nil, {
-	data = {min = 0, max = 20},
-	category = "characters"
+    data = {
+        min = 0,
+        max = 20
+    },
+    category = "characters"
 })
 
 nut.config.add("invH", 4, "How many slots in a column there is in a default inventory.", nil, {
-	data = {min = 0, max = 20},
-	category = "characters"
+    data = {
+        min = 0,
+        max = 20
+    },
+    category = "characters"
 })
 
 nut.config.add("minDescLen", 16, "The minimum number of characters in a description.", nil, {
-	data = {min = 0, max = 300},
-	category = "characters"
+    data = {
+        min = 0,
+        max = 300
+    },
+    category = "characters"
 })
 
 nut.config.add("saveInterval", 300, "How often characters save in seconds.", nil, {
-	data = {min = 60, max = 3600},
-	category = "characters"
+    data = {
+        min = 60,
+        max = 3600
+    },
+    category = "characters"
 })
 
 nut.config.add("walkSpeed", 130, "How fast a player normally walks.", function(oldValue, newValue)
-	for k, v in ipairs(player.GetAll())	do
-		v:SetWalkSpeed(newValue)
-	end
+    for k, v in ipairs(player.GetAll()) do
+        v:SetWalkSpeed(newValue)
+    end
 end, {
-	data = {min = 75, max = 500},
-	category = "characters"
+    data = {
+        min = 75,
+        max = 500
+    },
+    category = "characters"
 })
 
 nut.config.add("runSpeed", 235, "How fast a player normally runs.", function(oldValue, newValue)
-	for k, v in ipairs(player.GetAll())	do
-		v:SetRunSpeed(newValue)
-	end
+    for k, v in ipairs(player.GetAll()) do
+        v:SetRunSpeed(newValue)
+    end
 end, {
-	data = {min = 75, max = 500},
-	category = "characters"
+    data = {
+        min = 75,
+        max = 500
+    },
+    category = "characters"
 })
 
 nut.config.add("walkRatio", 0.5, "How fast one goes when holding ALT.", nil, {
-	form = "Float",
-	data = {min = 0, max = 1},
-	category = "characters"
+    form = "Float",
+    data = {
+        min = 0,
+        max = 1
+    },
+    category = "characters"
 })
 
 nut.config.add("punchStamina", 10, "How much stamina punches use up.", nil, {
-	data = {min = 0, max = 100},
-	category = "characters"
+    data = {
+        min = 0,
+        max = 100
+    },
+    category = "characters"
 })
 
 nut.config.add("defMoney", 0, "The amount of money that players start with.", nil, {
-	category = "characters",
-	data = {min = 0, max = 10000}
+    category = "characters",
+    data = {
+        min = 0,
+        max = 10000
+    }
 })
 
 nut.config.add("allowExistNames", true, "Whether or not players can use an already existing name upon character creation.", nil, {
-	category = "characters"
+    category = "characters"
 })
 
 nut.config.add("allowVoice", false, "Whether or not voice chat is allowed.", nil, {
-	category = "server"
+    category = "server"
 })
 
 nut.config.add("voiceDistance", 600.0, "How far can the voice be heard.", function(oldValue, newValue)
-	nut.config.squaredVoiceDistance = newValue * newValue
+    nut.config.squaredVoiceDistance = newValue * newValue
 end, {
-	form = "Float",
-	category = "server",
-	data = {min = 0, max = 5000}
+    form = "Float",
+    category = "server",
+    data = {
+        min = 0,
+        max = 5000
+    }
 })
 
 nut.config.add("contentURL", "http://nutscript.net/", "Your server's collection pack.", nil, {
-	category = "server"
+    category = "server"
 })
 
-nut.config.add(
-	"moneyModel",
-	"models/props_lab/box01a.mdl",
-	"The model for money entities.",
-	nil,
-	{category = "server"}
-)
+nut.config.add("moneyModel", "models/props_lab/box01a.mdl", "The model for money entities.", nil, {
+    category = "server"
+})
 
-nut.config.add(
-	"salaryInterval",
-	300,
-	"How often a player gets paid in seconds.",
-	nil,
-	{
-		data = {min = 1, max = 3600},
-		category = "characters"
-	}
-)
+nut.config.add("salaryInterval", 300, "How often a player gets paid in seconds.", nil, {
+    data = {
+        min = 1,
+        max = 3600
+    },
+    category = "characters"
+})
 
 local dist = nut.config.get("voiceDistance")
 nut.config.squaredVoiceDistance = dist * dist
