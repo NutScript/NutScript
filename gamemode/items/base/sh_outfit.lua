6cf4eb9183cd795f4d2bd9e07090c4821950b26f
TovarischPootis <54110479+TovarischPootis@users.noreply.github.com>
2022-10-26 13:52:57 +0200

    Outfit Demos
    Update the outfit demo .txt's (replacing the depricated pacoutfit with outfit) and adding spacing to the examples in the base for ease of reading

--- gamemode/items/base/sh_outfit.lua
+++ gamemode/items/base/sh_outfit.lua
@@ -13,10 +13,13 @@ ITEM.pacData = {}
 -- This will change a player's skin after changing the model.
 -- Keep in mind it starts at 0.
 ITEM.newSkin = 1
+
 -- This will change a certain part of the model.
 ITEM.replacements = {"group01", "group02"}
+
 -- This will change the player's model completely.
 ITEM.replacements = "models/manhack.mdl"
+
 -- This will have multiple replacements.
 ITEM.replacements = {
 	{"male", "female"},
@@ -77,7 +80,7 @@ function ITEM:removeOutfit(client)
 				end
 			end
 		end
-    
+
 		character:setData("oldGroups", oldGroups)
 	end
 
