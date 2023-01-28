933119c867fe393d74ecbb313b6a4109cc8fc834
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-01-28 20:59:56 +0000

    Fixed Vendor Crash Exploit
    This exploit allows you to spam items on the ground, making the server overload their net-flow, crashing it as a consequence.
    
    It is a simple exploit, but really effective on crashing servers.

--- plugins/vendor/sv_hooks.lua
+++ plugins/vendor/sv_hooks.lua
@@ -144,24 +144,23 @@ function PLUGIN:VendorTradeAttempt(
 		character:takeMoney(price)
 
 		vendor:takeStock(itemType)
+        local position = client:getItemDropPos()
 
-		local position = client:getItemDropPos()
-		local result = character:getInv():add(itemType)
-			:next(function(item)
-				hook.Run("OnCharTradeVendor", client, vendor, item, isSellingToVendor)
-				client.vendorTransaction = nil
-			end)
-			:catch(function(err)
-				if (IsValid(client)) then
-					client:notifyLocalized("itemOnGround")
-				end
-				client.vendorTransaction = nil
-				return nut.item.spawn(itemType, position)
-			end)
-			:catch(function(err)
-				client:notifyLocalized(err)
-				client.vendorTransaction = nil
-			end)
+        local result = character:getInv():add(itemType):next(function(item)
+            hook.Run("OnCharTradeVendor", client, vendor, item, isSellingToVendor)
+            client.vendorTransaction = nil
+        end):catch(function(err)
+            if IsValid(client) then
+                client:notifyLocalized("Cannot add to inventory! Giving money back!")
+            end
+
+            client.vendorTransaction = nil
+
+            return character:giveMoney(price)
+        end):catch(function(err)
+            client:notifyLocalized(err)
+            client.vendorTransaction = nil
+        end)
 
 		nut.log.add(client, "vendorBuy", itemType, vendor:getNetVar("name"))
 	end
