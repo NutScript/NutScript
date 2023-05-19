c37fb81e3563f08e312d96a01541af8243b6e2b2
TovarischPootis <54110479+TovarischPootis@users.noreply.github.com>
2023-05-19 11:39:30 +0200

    1.2.3 (#139)
    * Storage save extra stuff
    
    Save Skin, bodygroups, colour and material
    
    * Vendors save extra stuff
    
    Save bodygroups, skin, colour, material

--- plugins/vendor/sv_data.lua
+++ plugins/vendor/sv_data.lua
@@ -3,12 +3,20 @@ local PLUGIN = PLUGIN
 function PLUGIN:saveVendors()
 	local data = {}
 		for k, v in ipairs(ents.FindByClass("nut_vendor")) do
+			local groups = {}
+			for _, groupData in pairs(v:GetBodyGroups()) do
+				groups[groupData.id] = v:GetBodygroup(groupData.id)
+			end
 			data[#data + 1] = {
 				name = v:getNetVar("name"),
 				desc = v:getNetVar("desc"),
 				pos = v:GetPos(),
 				angles = v:GetAngles(),
 				model = v:GetModel(),
+				skin = v:GetSkin() or 0,
+				groups = groups,
+				color = v:GetColor(),
+				material = v:GetMaterial(),
 				bubble = v:getNetVar("noBubble"),
 				items = v.items,
 				factions = v.factions,
@@ -18,6 +26,10 @@ function PLUGIN:saveVendors()
 			}
 		end
 	self:setData(data)
+end
+
+function PLUGIN:SaveData()
+	self:saveVendors()
 end
 
 function PLUGIN:LoadData()
@@ -32,6 +44,12 @@ function PLUGIN:LoadData()
 		entity:SetAngles(v.angles)
 		entity:Spawn()
 		entity:SetModel(v.model)
+		entity:SetSkin(v.skin or 0)
+		for groupID, groupValue in pairs(v.groups or {}) do
+			entity:SetBodygroup(groupID, groupValue)
+		end
+		entity:SetColor(v.color or Color(255, 255, 255))
+		entity:SetMaterial(v.material or "")
 		entity:setNetVar("noBubble", v.bubble)
 		entity:setNetVar("name", v.name)
 		entity:setNetVar("desc", v.desc)
